0db598c4214987fded5d1a4ed1e8dc6f295747f6
mdcoolcat <danielle.mei1989@gmail.com>
2013-04-23 22:24:12 -0400

    Change MSGTYPE to enum with interface for converting between string

--- webcam-capture-example-live-streaming/src/main/java/us/sosia/video/stream/agent/messaging/MSGTYPE.java
+++ webcam-capture-example-live-streaming/src/main/java/us/sosia/video/stream/agent/messaging/MSGTYPE.java
@@ -1,14 +1,35 @@
 package us.sosia.video.stream.agent.messaging;
 
-public final class MSGTYPE {
-	public static final String TOKEN = "TOKEN";
-	public static final String BEGIN = "BEGIN";
-	public static final String ROUNDFINISHED = "ROUNDFINISHED";
-	public static final String ANSWER = "ANSWER";
-	public static final String SCOREUPDATE = "SCOREUPDATE";
-	//TODO more types
-	
-	private MSGTYPE() {
-	
+import java.util.HashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+
+public enum MSGTYPE {
+	TOKEN("TOKEN"), BEGIN("BEGIN"), ROUNDFINISHED("ROUNDFINISHED"), ANSWER("ANSWER"), SCOREUPDATE("SCOREUPDATE"), FAILED("FAILED");
+//	 TODO more types
+
+	private final String	name;
+	private static final Map<String, MSGTYPE> map = new HashMap<String, MSGTYPE>();
+	static {
+		for (MSGTYPE t : MSGTYPE.values())
+			map.put(t.name, t);
+	}
+
+	private MSGTYPE(String name) {
+		this.name = name;
+	}
+
+	public static MSGTYPE fromString(String name) {
+		if (map.containsKey(name))
+			return map.get(name);
+		throw new NoSuchElementException(name+" not found.");
+	}
+//	public boolean equalsType(String otherType) {
+//		return (otherType == null) ? false : type.equals(otherType);
+//	}
+
+	@Override
+	public String toString() {
+		return name;
 	}
 }
